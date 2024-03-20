ddfc0c1b213774158dbed60105c6d3d45af4d461
Mine Rala <mine-rala@hotmail.com>
2024-03-20 11:36:35 +0300

    Initial Commit

---
+++ AirbnbAppCloneSwiftUI/AirbnbAppCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbAppCloneSwiftUIApp.swift
+//  AirbnbAppCloneSwiftUI
+//
+//  Created by Mine Rala on 20.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbAppCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
